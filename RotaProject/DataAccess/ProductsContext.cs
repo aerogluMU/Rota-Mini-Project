4e635b3a84499095fdf9c8e60471dc1448df619b
Alperen Eroğlu <73244893+aerogluMU@users.noreply.github.com>
2023-07-11 01:22:52 +0300

    Add files via upload

---
+++ RotaProject/DataAccess/ProductsContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RotaProject.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotaProject.DataAccess
+{
+    public class ProductsContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RotaMiniProj;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(c => c.Category)
+                .WithMany(d => d.Products);
+        }
+
+    }
+}
---
+++ RotaProject/DataModels/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotaProject.DataModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float StockBalance { get; set; }
+        public Category Category { get; set; }
+    }
+}
